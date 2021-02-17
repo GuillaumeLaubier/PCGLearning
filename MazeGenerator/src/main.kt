a828b15f9013b2f3dfa0d3260dd0320ff5c6625c
Guillaume Laubier <glaubier@smartadserver.com>
2021-02-17 17:58:34 +0100

    Start maze generator

---
+++ MazeGenerator/src/main.kt
@@ -0,0 +1,22 @@
+import javafx.embed.swing.SwingFXUtils
+import javafx.scene.image.Image
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import javax.imageio.ImageIO
+
+fun main() {
+
+}
+
+fun writeImage(image: Image, algoName: String) {
+    val byteOutput = ByteArrayOutputStream()
+
+    val timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+    val file = File("results/$algoName/$timestamp.png")
+
+    ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", byteOutput)
+
+    file.writeBytes(byteOutput.toByteArray())
+}
