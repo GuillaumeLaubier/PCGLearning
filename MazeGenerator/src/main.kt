84e6c30ddf6dc76f69bc98c57a59ed875b3dcbe4
Guillaume Laubier <glaubier@smartadserver.com>
2021-02-17 18:16:05 +0100

    Design cell image

--- MazeGenerator/src/main.kt
+++ MazeGenerator/src/main.kt
@@ -1,5 +1,6 @@
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.Image
+import model.MazeCell
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.time.Instant
@@ -7,14 +8,14 @@ import java.time.format.DateTimeFormatter
 import javax.imageio.ImageIO
 
 fun main() {
-
+    writeImage(MazeCell().getImage())
 }
 
-fun writeImage(image: Image, algoName: String) {
+fun writeImage(image: Image) {
     val byteOutput = ByteArrayOutputStream()
 
     val timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
-    val file = File("results/$algoName/$timestamp.png")
+    val file = File("results/$timestamp.png")
 
     ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", byteOutput)
 
--- MazeGenerator/src/model/MazeCell.kt
+++ MazeGenerator/src/model/MazeCell.kt
@@ -1,2 +1,51 @@
 package model
 
+import javafx.scene.image.Image
+import javafx.scene.image.PixelWriter
+import javafx.scene.image.WritableImage
+import javafx.scene.paint.Color
+
+class MazeCell() {
+
+    val width = 40
+    val height = 40
+
+    var isVisited = false
+
+    val hasTopWall = true
+    val hasBottomWall = true
+    val hasLeftWall = true
+    val hasRightWell = true
+
+    fun getImage(): Image {
+        val writableImage = WritableImage(width, height)
+
+        draw(writableImage.pixelWriter, 0, width - 1, 0, height - 1, Color.WHITE)
+
+        if (hasTopWall) {
+            draw(writableImage.pixelWriter, 0, width - 1, 0, (height / 4) - 1, Color.BLACK)
+        }
+
+        if (hasBottomWall) {
+            draw(writableImage.pixelWriter, 0, width - 1, (height / 4) * 3, height - 1, Color.BLACK)
+        }
+
+        if (hasLeftWall) {
+            draw(writableImage.pixelWriter, 0, (width / 4) - 1, 0, height - 1, Color.BLACK)
+        }
+
+        if (hasRightWell) {
+            draw(writableImage.pixelWriter, (width / 4) * 3, width - 1, 0, height - 1, Color.BLACK)
+        }
+
+        return writableImage
+    }
+
+    private fun draw(pixelWriter: PixelWriter, xStart: Int, xEnd: Int, yStart: Int, yEnd: Int, color: Color) {
+        for (x in xStart..xEnd) {
+            for (y in yStart..yEnd) {
+                pixelWriter.setColor(x, y, color)
+            }
+        }
+    }
+}
